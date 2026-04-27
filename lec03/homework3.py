f3c15b39c2a28f5e51e719b621e8755bdabd3776
Qiankunyi <qiankunyi7@gmail.com>
2026-04-27 11:35:38 +0900

    Update homework3.py

--- lec03/homework3.py
+++ lec03/homework3.py
@@ -14,5 +14,36 @@ def words2characters(words):
     example, if words==['hello', 1.234, True], then characters should be
     ['h', 'e', 'l', 'l', 'o', '1', '.', '2', '3', '4', 'T', 'r', 'u', 'e']
     """
-    return []
+    characters = []
+    for item in words:
+        item_str = str(item)
+        for char in item_str:
+            characters.append(char)
+    return characters
 
+
+def cancellation(input_list, stop_word):
+
+    output_list = []
+    for item in input_list:
+        if item == stop_word:
+            break
+        else:
+            output_list.append(item)
+    return output_list
+
+
+def copy_all_but_skip_word(input_list, skip_word):
+
+    output_list = []
+    for item in input_list:
+        if item == skip_word:
+            continue
+        else:
+            output_list.append(item)
+    return output_list
+
+
+def my_average(input_list):
+
+    return sum(input_list) / len(input_list)
