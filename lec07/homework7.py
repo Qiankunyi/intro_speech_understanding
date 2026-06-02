736de42f5cfcb0b194ac209d325bb831cc0cccaa
Qiankunyi <qiankunyi7@gmail.com>
2026-06-02 16:59:36 +0900

    Update homework7.py

--- lec07/homework7.py
+++ lec07/homework7.py
@@ -16,7 +16,21 @@ def major_chord(f, Fs):
     (2) A major third, i.e., four semitones above f
     (3) A major fifth, i.e., seven semitones above f
     '''
-    raise RuntimeError("You need to write this part")
+    num_s = int(0.5 * Fs)
+
+    n = np.arange(num_s)
+
+    f1 = f                          
+    f2 = f * np.power(2, 4/12)     
+    f3 = f * np.power(2, 7/12)
+
+    x1 = np.cos(2 * np.pi * f1 * n / Fs)
+    x2 = np.cos(2 * np.pi * f2 * n / Fs)
+    x3 = np.cos(2 * np.pi * f3 * n / Fs)
+
+    x = x1 + x2 + x3
+
+    return x
 
 def dft_matrix(N):
     '''
@@ -29,7 +43,14 @@ def dft_matrix(N):
     W (NxN array): a matrix of dtype='complex' whose (k,n)^th element is:
            W[k,n] = cos(2*np.pi*k*n/N) - j*sin(2*np.pi*k*n/N)
     '''
-    raise RuntimeError("You need to write this part")
+    W = np.zeros((N, N), dtype=complex)
+
+    for k in range(N):
+        for n in range(N):
+            
+            W[k, n] = np.cos(2 * np.pi * k * n / N) - 1j * np.sin(2 * np.pi * k * n / N)
+            
+    return W
 
 def spectral_analysis(x, Fs):
     '''
@@ -43,4 +64,26 @@ def spectral_analysis(x, Fs):
     f1, f2, f3: The three loudest frequencies (in Hertz)
       These should be sorted so f1 < f2 < f3.
     '''
-    raise RuntimeError("You need to write this part")
+    N = len(x)
+
+    X = np.fft.fft(x)
+
+    X_m = np.abs(X)
+
+    half_N = N // 2
+    pos_m = X_m[:half_N]
+
+    top_3_k = np.argsort(pos_m)[-3:][::-1]
+
+    fre = []
+    for k in top_3_k:
+        f = (k / N) * Fs
+        fre.append(f)
+    
+    fre_sorted = sorted(fre)
+
+    f1 = fre_sorted[0]
+    f2 = fre_sorted[1]
+    f3 = fre_sorted[2]
+
+    return f1, f2, f3
