362b7c064592eb6ed0a864fb7ffdf9d21a050eb3
Qiankunyi <qiankunyi7@gmail.com>
2026-07-27 11:06:03 +0900

    lec14 homework done

--- lec14/homework14.py
+++ lec14/homework14.py
@@ -9,7 +9,8 @@ def synthesize(text, lang, filename):
     lang (str) - the language in which you want to synthesize it
     filename (str) - the filename in which it should be saved
     '''
-    raise RuntimeError("You need to write this!")
+    tts = gtts.gTTS(text=text, lang=lang)
+    tts.save(filename)
 
 def make_a_corpus(texts, languages, filenames):
     '''
@@ -24,5 +25,23 @@ def make_a_corpus(texts, languages, filenames):
     @return:
     recognized_texts - list of the strings that were recognized from each file
     '''
-    raise RuntimeError("You need to write this!")
+    recognized_texts = []
+    r = speech_recognition.Recognizer()
+    
+    for text, lang, filename in zip(texts, languages, filenames):
+        
+        mp3_file = filename + ".mp3"
+        wav_file = filename + ".wav"
+        synthesize(text, lang, mp3_file)
+        
+        data, samplerate = librosa.load(mp3_file)
+        soundfile.write(wav_file, data, samplerate)
+        
+        with speech_recognition.AudioFile(wav_file) as source:
+            audio = r.record(source)
+            recognized = r.recognize_google(audio, language=lang)
+        
+        recognized_texts.append(recognized)
+    
+    return recognized_texts
         
